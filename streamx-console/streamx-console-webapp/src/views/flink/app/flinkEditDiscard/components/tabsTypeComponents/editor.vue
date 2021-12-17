7c8d429366e0b8df1a1f4887fb6dd672d2d53853
zhanglong <zhanglong6740@163.com>
2021-12-17 09:21:59 +0800

    废弃编辑器页面

---
+++ streamx-console/streamx-console-webapp/src/views/flink/app/flinkEditDiscard/components/tabsTypeComponents/editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import {
+  applyPom,
+  bigScreenClose,
+  bigScreenOk,
+  bigScreenOpen,
+  formatSql,
+  initEditor,
+  initPodTemplateEditor,
+  updateDependency,
+  verifySQL
+} from '../../../AddEdit'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
