38371ffc98e1efbc135b8a9851bb1a5cc365761b
邵龙 <370440668@qq.com>
2022-03-30 13:54:12 +0800

    fix: mognoDB示例代码格式不对

--- streamx-console/streamx-console-webapp/src/views/flink/app/flinkEdit/constant.js
+++ streamx-console/streamx-console-webapp/src/views/flink/app/flinkEdit/constant.js
@@ -8,7 +8,7 @@ export const DATA_SOURCE = [
     name: 'ClickHouse',
     code: `CREATE TABLE table1 (id String,name String
 ) WITH  (
-    'connector' = 'jdbc', 
+    'connector' = 'jdbc',
     'url' = 'jdbc:clickhouse://localhost:8123',
     'table-name' = 'demo_test' ,
     'username' = 'default' ,
@@ -21,15 +21,15 @@ INSERT INTO table2 SELECT id, name FROM table1;`
   {
     key: 'Dameng',
     name: '达梦数据库',
-    code: `CREATE TABLE table1 ( id string,name string 
-) WITH  ( 
-       'connector' = 'jdbc',     
-       'url' = 'jdbc:dm://localhost:5236/SYSDBA',    
-       'table-name' = 'TEST' ,    
-       'username' = 'SYSDBA' ,    
+    code: `CREATE TABLE table1 ( id string,name string
+) WITH  (
+       'connector' = 'jdbc',
+       'url' = 'jdbc:dm://localhost:5236/SYSDBA',
+       'table-name' = 'TEST' ,
+       'username' = 'SYSDBA' ,
        'password' = 'SYSDBA');
-CREATE TABLE table2 ( id string,name string 
-) WITH  ( 
+CREATE TABLE table2 ( id string,name string
+) WITH  (
        'connector' = 'print');
 INSERT INTO table2 SELECT  id,name  FROM table1;`
   },
@@ -37,14 +37,14 @@ INSERT INTO table2 SELECT  id,name  FROM table1;`
     key: 'Elasticsearch',
     name: 'Elasticsearch',
     code: `CREATE TABLE table1 (id String,name String
-  'connector' = 'kafka',   
+  'connector' = 'kafka',
   'topic' = 'test',
-  'properties.bootstrap.servers' = 'localhost:9092',   
+  'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'demo-test-group');
 CREATE TABLE table2 (id String,name String
-) WITH ( 
-   'connector' = 'elasticsearch-7', 
-   'hosts' = 'http://localhost:9200', 
+) WITH (
+   'connector' = 'elasticsearch-7',
+   'hosts' = 'http://localhost:9200',
    'username' = 'elastic',
    'password' = 'elastic123456',
    'index' = 'demo-test' );
@@ -54,14 +54,14 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'Greenplum',
     name: 'Greenplum',
     code: `CREATE TABLE table1 (id string,name string
-) WITH   ( 
-  'connector' = 'jdbc',     
-  'url' = 'jdbc:postgresql://localhost:2345/greenplum',    
-  'table-name' = 'test' ,    
-  'username' = 'gpadmin' ,    
+) WITH   (
+  'connector' = 'jdbc',
+  'url' = 'jdbc:postgresql://localhost:2345/greenplum',
+  'table-name' = 'test' ,
+  'username' = 'gpadmin' ,
   'password' = 'gpadmin' );
 CREATE TABLE table2 (id string,name string
-) WITH   ( 
+) WITH   (
   'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -69,15 +69,15 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'HBase',
     name: 'HBase',
     code: `CREATE TABLE  table1 ( id string,name string
-) WITH ( 
-       'connector' = 'kafka',   
+) WITH (
+       'connector' = 'kafka',
        'topic' = 'test',
-       'properties.bootstrap.servers' = 'localhost:9092',   
+       'properties.bootstrap.servers' = 'localhost:9092',
        'properties.group.id' = 'demo-test-group');
-CREATE TABLE table2 ( id string,name string 
+CREATE TABLE table2 ( id string,name string
 ) WITH  (
-       'connector' = 'hbase-2.2',  
-       'table-name' = 'mytable',  
+       'connector' = 'hbase-2.2',
+       'table-name' = 'mytable',
        'zookeeper.quorum' = 'localhost:2181' ) ;
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -96,12 +96,12 @@ INSERT INTO print SELECT * FROM test;`
     key: 'Kafka',
     name: 'Kafka',
     code: `CREATE TABLE table1 (id String,name String
-  'connector' = 'kafka',   
+  'connector' = 'kafka',
   'topic' = 'test',
-  'properties.bootstrap.servers' = 'localhost:9092',   
+  'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'demo-test-group');
 CREATE TABLE table2 (id String,name String
-) WITH ( 
+) WITH (
    'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -109,16 +109,16 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'MongoDB',
     name: 'MongoDB',
     code: `CREATE TABLE  table1 ( id string,name string
-) WITH ( 
-       'connector' = 'kafka',   
+) WITH (
+       'connector' = 'kafka',
        'topic' = 'test',
-       'properties.bootstrap.servers' = 'localhost:9092',   
-       'properties.group.id' = 'demo-test-group'');
-CREATE TABLE table2 ( id string,name string 
-) WITH  ( 
-       'connector' = 'mongodb',  
-     'host' = 'mongodb://localhost:27017',  
-     'database' = 'test', 
+       'properties.bootstrap.servers' = 'localhost:9092',
+       'properties.group.id' = 'demo-test-group');
+CREATE TABLE table2 ( id string,name string
+) WITH  (
+       'connector' = 'mongodb',
+     'host' = 'mongodb://localhost:27017',
+     'database' = 'test',
      'collection' = 'test') ;
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -126,14 +126,14 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'MySQL',
     name: 'MySQL',
     code: `CREATE TABLE table1 (id string,name string
-) WITH   (   
-       'connector' = 'jdbc',     
-       'url' = 'jdbc:mysql://localhost:3306/mysql',    
-       'table-name' = 'test' ,    
+) WITH   (
+       'connector' = 'jdbc',
+       'url' = 'jdbc:mysql://localhost:3306/mysql',
+       'table-name' = 'test' ,
        'username' = 'demo' ,
        'password' = 'test1234'	);
 CREATE TABLE table2 (id string,name string
-) WITH   (   
+) WITH   (
        'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -141,14 +141,14 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'Oracle',
     name: 'Oracle',
     code: `CREATE TABLE table1 (id String,name String
-)  WITH ( 
+)  WITH (
        'connector' = 'jdbc',
-       'url' = 'jdbc:oracle:thin:@localhost:1521:orcl', 
+       'url' = 'jdbc:oracle:thin:@localhost:1521:orcl',
        'username' = 'oracle',
-       'password' = 'oracle1234', 
+       'password' = 'oracle1234',
        'table-name' = 'test');
 CREATE TABLE table2 (id String,name String
-)  WITH ( 
+)  WITH (
        'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -156,14 +156,14 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'PostgreSQL',
     name: 'PostgreSQL',
     code: `CREATE TABLE table1 (id string,name string
-) WITH   ( 
-  'connector' = 'jdbc',     
-  'url' = 'jdbc:postgresql://localhost:5432/postgres',    
-  'table-name' = 'test' ,    
-  'username' = 'postgres' ,    
+) WITH   (
+  'connector' = 'jdbc',
+  'url' = 'jdbc:postgresql://localhost:5432/postgres',
+  'table-name' = 'test' ,
+  'username' = 'postgres' ,
   'password' = 'postgres' );
 CREATE TABLE table2 (id string,name string
-) WITH   ( 
+) WITH   (
   'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -171,14 +171,14 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'SQLServer',
     name: 'SQLServer',
     code: `CREATE TABLE table1 (id string,name string
-) WITH   (  
-       'connector' = 'jdbc',     
-       'url' = 'jdbc:sqlserver://localhost;databaseName=DataTest',    
-       'table-name' = 'dbo.test' ,    
-       'username' = 'sa' ,    
+) WITH   (
+       'connector' = 'jdbc',
+       'url' = 'jdbc:sqlserver://localhost;databaseName=DataTest',
+       'table-name' = 'dbo.test' ,
+       'username' = 'sa' ,
        'password' = 'test123' );
 CREATE TABLE table2 (id string,name string
-) WITH   (  
+) WITH   (
        'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   },
@@ -186,13 +186,13 @@ INSERT INTO table2 SELECT id,name FROM table1;`
     key: 'TiDB',
     name: 'TiDB',
     code: `CREATE TABLE table1 (id string,name string
-) WITH   (   
-       'connector' = 'jdbc',     
-       'url' = 'jdbc:mysql://localhost:4000/test',    
-       'table-name' = 'test' ,    
+) WITH   (
+       'connector' = 'jdbc',
+       'url' = 'jdbc:mysql://localhost:4000/test',
+       'table-name' = 'test' ,
        'username' = 'root'  );
 CREATE TABLE table2 (id string,name string
-) WITH   (   
+) WITH   (
        'connector' = 'print');
 INSERT INTO table2 SELECT id,name FROM table1;`
   }
@@ -231,7 +231,7 @@ CONCAT(string1, string2,...)`
   {
     key: '字段内容清洗',
     name: '字段内容清洗',
-    code: `LTRIM(string)  
+    code: `LTRIM(string)
 RTRIM(string)`
   },
   {
